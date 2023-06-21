bb1ee0062dfd1b62b244560c78eb385ec1de6255
AdamArabov <arabovadam@yahoo.com>
2023-06-21 18:52:19 -0400

    trimming the fat

--- WitcherSite/src/pages/index.tsx
+++ WitcherSite/src/pages/index.tsx
@@ -14,6 +14,8 @@ export default function Home() {
   // adaadadadadadadadadadadadadadadadadadaadadadadadadadadadad
   // adadadaddadadadadadadadadadadadaadadadadaad
   // adadadaddadadadadadadadadadadadaadadadadaad
+  // adadadaddadadadadadadadadadadadaadadadadaad
+  // adadadaddadadadadadadadadadadadaadadadadaad
 
 
 }
