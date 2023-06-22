178c053542e88a4216ff1ef59f4d3c3304417d69
AdamArabov <arabovadam@yahoo.com>
2023-06-22 18:02:18 -0400

    Profile photo appears

--- WitcherSite/src/pages/index.tsx
+++ WitcherSite/src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import data from '../data.json'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -6,6 +7,13 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
         <div className='flex items-center'>
+          <Image
+          className='rounded-full'
+          alt={data.name}
+          src={data.avatar}
+          width={120}
+          height={120}
+          />
         </div>
     )
 }
