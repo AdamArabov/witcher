ae184b54e179b09c9cab63b0f686a9ac3779310d
AdamArabov <arabovadam@yahoo.com>
2023-06-23 12:13:52 -0400

    Designed the links

--- WitcherSite/src/pages/index.tsx
+++ WitcherSite/src/pages/index.tsx
@@ -12,9 +12,11 @@ url:string;
 title:string
 }){
   return(
-    <a href= {url}>
-    <div className='flex flex-col'>
-    <h2 className='font-bold text-lg'>{title}</h2>
+    <a href= {url}
+    className='flex items-center p-4 rounded-md hover:scale-105 transition-all w-full border border-white-100 mb-3'
+    >
+    <div className='flex flex-col w-full text-center'>
+    <h2 className='font-semibold text-lg '>{title}</h2>
     </div>
     </a>
   )
@@ -22,7 +24,7 @@ title:string
 
 export default function Home() {
   return (
-        <div className='flex items-center flex-col mx-auto w-full justify-center mt-16'>
+        <div className='flex items-center flex-col mx-auto w-full justify-center mt-16 px-8'>
           <Image
           className='rounded-full'
           alt={data.name}
@@ -30,7 +32,7 @@ export default function Home() {
           width={96}
           height={96}
           />
-          <h1 className='mt-4 font-bold text-xl'>{data.name}</h1>
+          <h1 className='mt-4 mb-8 font-bold text-xl '>{data.name}</h1>
           {data.links.map((link)=> (
             <LinkCard key={link.url} {...link} />
           ))}
