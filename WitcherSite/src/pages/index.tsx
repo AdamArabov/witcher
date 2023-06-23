f5b9de2c96711591799f53cbc54e67dd479f9e55
AdamArabov <arabovadam@yahoo.com>
2023-06-23 11:26:28 -0400

    Links work

--- WitcherSite/src/pages/index.tsx
+++ WitcherSite/src/pages/index.tsx
@@ -4,6 +4,22 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
+function LinkCard ({
+  title,
+  url
+}: {
+url:string;
+title:string
+}){
+  return(
+    <a href= {url}>
+    <div className='flex flex-col'>
+    <h2 className='font-bold text-lg'>{title}</h2>
+    </div>
+    </a>
+  )
+}
+
 export default function Home() {
   return (
         <div className='flex items-center flex-col mx-auto w-full justify-center mt-16'>
@@ -15,6 +31,11 @@ export default function Home() {
           height={96}
           />
           <h1 className='mt-4 font-bold text-xl'>{data.name}</h1>
+          {data.links.map((link)=> (
+            <LinkCard key={link.url} {...link} />
+          ))}
         </div>
     )
 }
+
+
