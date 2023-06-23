8d96a2cd87726c31f558b469a422a3094e20abbb
AdamArabov <arabovadam@yahoo.com>
2023-06-23 11:14:59 -0400

    header done

--- WitcherSite/src/pages/index.tsx
+++ WitcherSite/src/pages/index.tsx
@@ -6,15 +6,15 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-        <div className='flex mx-auto w-full justify-center mt-16'>
+        <div className='flex items-center flex-col mx-auto w-full justify-center mt-16'>
           <Image
           className='rounded-full'
           alt={data.name}
           src={data.avatar}
-          width={80}
-          height={80}
+          width={96}
+          height={96}
           />
-          <h1 className='font-semibold'>{data.name}</h1>
+          <h1 className='mt-4 font-bold text-xl'>{data.name}</h1>
         </div>
     )
 }
