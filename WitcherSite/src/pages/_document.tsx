e94ec33fcf4897e87efb807c4d11a7ed5721daf7
AdamArabov <arabovadam@yahoo.com>
2023-06-23 12:16:29 -0400

    added bg

--- WitcherSite/src/pages/_document.tsx
+++ WitcherSite/src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className='bg-gradient-to-b from-gray-900 via-purple-900 to-violet-600'>
         <Main />
         <NextScript />
       </body>
